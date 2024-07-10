30fe515fa45a84e37df9512ae4dfd217bd943e31
PetrCala <cala.p@seznam.cz>
2024-07-10 09:08:13 +0200

    Modified the name of the logging options

--- r/libs/logs/index.R
+++ r/libs/logs/index.R
@@ -5,9 +5,9 @@ box::use(
 
 #' Setup logging for the project
 setup_logging <- function() {
-  log_to_console <- METADATA$options$log_to_console
+  log_to_console_only <- METADATA$options$log_to_console_only
 
-  if (!log_to_console) {
+  if (!log_to_console_only) {
     log_file <- file.path(PATHS$DIR_R, METADATA$options$log_file_name)
 
     logger::log_appender(appender = logger::appender_file(log_file, max_files = 1L))
