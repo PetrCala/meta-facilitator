d48ab050ca9b85c8e0093c0ac4e3aa165301694a
PetrCala <cala.p@seznam.cz>
2024-07-31 11:06:39 +0200

    Clean up lint issues

--- r/testing/test_utils.R
+++ r/testing/test_utils.R
@@ -1,4 +1,4 @@
-setup_test_console_logger <- function(){
+setup_test_console_logger <- function() {
   logger::set_log_level("info")
   logger::set_log_output("console")
   logger::set_log_format("[%l] %v")
