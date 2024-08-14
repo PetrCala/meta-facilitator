10ca7718cf2e4c97bce67db7da1219b0ffe09bbd
Petr Čala <cala.p@seznam.cz>
2024-08-14 15:19:25 +0200

    Pull the latest changes from 'dev' (#2)
    * Fix lint issues
    
    * Add a list of dev packages
    
    * The install and check package version is now set to NA by default (latest package version)
    
    * Export the setup_env function
    
    * Add an editorconfig file
    
    * Remove editorconfig
    
    * Modify the environment and package handling
    
    * Add GitHub Actions workflow for lint check
    
    * Improve the lint shell script
    
    * Add lintr package installation to the lint workflow
    
    * Assume tips and utilities for the lint workflow from r-lib examples
    
    * Modify the lint action to run only on opened master PRs
    
    * Modify option loading

--- r/base/options.R
+++ r/base/options.R
@@ -1,17 +1,37 @@
 box::use(
   yaml,
-  base / paths[PATHS]
+  base / paths[PATHS],
+  base / const[CONST]
 )
 
-#' Read the options.json file and return it as a list
-read_options <- function() {
-  if (!file.exists(PATHS$R_OPTIONS_YAML)) {
-    rlang::abort("options.json file not found")
+#' Recursively parse the YAML structure and set options with a prefix
+#'
+#' @param options_list A list of options
+#' @param prefix A prefix to use for the options
+#' @return NULL
+set_prefixed_options <- function(options_list, prefix) {
+  config_file <- PATHS$R_CONFIG_YAML
+  if (!file.exists(config_file)) {
+    rlang::abort(paste(config_file, "file not found"))
   }
-  options <- yaml::read_yaml(PATHS$R_OPTIONS_YAML)
-  return(options)
+  for (name in names(options_list)) {
+    value <- options_list[[name]]
+    if (is.list(value)) {
+      # If the value is a list, recursively process it
+      set_prefixed_options(value, paste(prefix, name, sep = "."))
+    } else {
+      # Set the option with the full prefixed name
+      option_name <- paste(prefix, name, sep = ".")
+      options(option_name = value)
+    }
+  }
 }
 
-#' A constant holding the options
+#' @title Options
+#' @description
+#' This object contains the options that are used in the project.
 #' @export
-OPTIONS <- read_options()
+OPTIONS <- yaml::read_yaml(PATHS$R_CONFIG_YAML)
+
+# Set options with the package name prefix
+set_prefixed_options(OPTIONS, CONST$PACKAGE_NAME)
